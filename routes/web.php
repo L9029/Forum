69be3ca584c31596ed455720889dfbea726f7c7d
L9029 <angeloca03@gmail.com>
2025-09-05 15:46:36 -0400

    Se procedio a configurar Livewire y a crear el componente que se encargara del sistema de preguntas del Forum.

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\ShowThreads;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', ShowThreads::class)
+    ->middleware('auth')
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
