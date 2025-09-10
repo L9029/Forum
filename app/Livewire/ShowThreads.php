2599ccea7790c6051474e72d3192ceb10069f6b4
L9029 <angeloca03@gmail.com>
2025-09-10 10:16:08 -0400

    Se procedio a crear el listado de los hilos del forum junto con su respectivo diseño en la pagina principal del Forum.

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -4,15 +4,18 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Models\Thread;
 
 class ShowThreads extends Component
 {
     public function render()
     {
         $categories = Category::get();
+        $threads = Thread::get();
 
         return view('livewire.show-threads', [
             'categories' => $categories,
+            'threads' => $threads,
         ]);
     }
 }
