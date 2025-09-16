72354bfc1bf436b2cada15972ec949a690dff153
L9029 <angeloca03@gmail.com>
2025-09-16 15:53:07 -0400

    Se procedio a crear el diseño del formulario que servira tanto para editar como para crear un pregunta.

--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -1,3 +1,13 @@
 <div>
-    Formulario
+    <select name="category_id" id="category_id" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4">
+        <option value="">Seleccione una Categoria</option>
+
+        @foreach($categories as $category)
+            <option value="{{ $category->id }}" @selected($category->id == $thread->category_id) >{{ $category->name }}</option>
+        @endforeach
+    </select>
+
+    <input type="text" name="title" placeholder="Título" value="{{ $thread->title }}" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4">
+
+    <textarea name="body" id="body" rows="10" placeholder="Descripción del problema" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4">{{ $thread->body }}</textarea>
 </div>
